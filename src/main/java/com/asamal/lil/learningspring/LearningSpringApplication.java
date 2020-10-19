8d1ffde6b47671534e7834494dbbc01dee5901bc
Ayaskanta Samal <silu.samal@gmail.com>
2020-10-18 21:40:01 -0400

    second

--- src/main/java/com/asamal/lil/learningspring/LearningSpringApplication.java
+++ src/main/java/com/asamal/lil/learningspring/LearningSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningSpringApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringApplication.class, args);
 	}
